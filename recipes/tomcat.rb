6ebcc660b28a4f02e44feb8d42581bb9aff7b1ab
DavidEugenePratt <davidpratt@foxhead.com>
2018-05-04 16:28:15 -0700

    Adds tomcat stuff

--- recipes/tomcat.rb
+++ recipes/tomcat.rb
@@ -5,10 +5,26 @@
 # Copyright:: 2018, The Authors, All Rights Reserved.
 
 tomcatversion = node.read('gantbox', 'tomcat', 'version') ? node['gantbox']['tomcat']['version'] : '8.5.30'
+tomcatusers = node.read('gantbox', 'tomcat', 'users') ? node['gantbox']['tomcat']['users'] : [ { "username" => "admin" , "password" => "password" } ]
 
 tomcat_install 'gant' do
   verify_checksum false
   version tomcatversion
+end
+
+template '/opt/tomcat_gant/webapps/manager/META-INF/context.xml' do
+  source 'context.xml.erb'
+  owner 'tomcat_gant'
+  group 'tomcat_gant'
+  mode '640'
+end
+
+template '/opt/tomcat_gant/conf/tomcat-users.xml' do
+  source 'tomcat-users.xml.erb'
+  owner 'tomcat_gant'
+  group 'tomcat_gant'
+  mode '600'
+  variables(:users => node['app']['max_cache_size'])
 end
 
 tomcat_service 'gant' do
